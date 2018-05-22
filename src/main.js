fe3ebafd2a53af8a4ac41a1ab53916739bb7fa5f
BolgarMaxym97 <bolgar.maxym@gmail.com>
2018-05-22 17:22:34 +0300

    upload

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VTooltip from 'v-tooltip'
+
+Vue.use(VTooltip);
+new Vue({
+  el: '#app',
+  render: h => h(App)
+});
+
+$(document).ready(function () {
+  $('.dataTable').DataTable({
+    "language": {
+      "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Russian.json"
+    },
+    "columnDefs": [{
+      "targets": 2,
+      "orderable": false
+    }]
+  });
+});
