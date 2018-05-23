94027d7952cc674de4b5abdb8d77a7e89807e79b
BolgarMaxym97 <bolgar.maxym@gmail.com>
2018-05-23 11:52:01 +0300

    info modal added

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
   <div id="app" class="h-100" style="position: relative">
     <div class="container">
       <div class="row justify-content-center align-items-center">
+        <div class="topcorner">
+          <button @click="showInfo" class="btn btn-info waves-effect waves-light btn-sm">
+            <i class="fas fa-info-circle"></i>
+          </button>
+        </div>
         <div class="col-md-12">
           <h1 class="display-1"><span class="title-text"> {{word}} - <span v-html="translated"></span> </span></h1>
           <button v-tooltip="okText" type="button"
@@ -91,6 +96,28 @@
               </div>
             </div>
 
+            <div class="modal" id="info">
+              <div class="modal-dialog modal-sm modal-dialog-centered">
+                <div class="modal-content">
+                  <!-- Modal Header -->
+                  <div class="modal-header">
+                    <h4 class="modal-title">Статистика</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  </div>
+                  <!-- Modal body -->
+                  <div class="modal-body">
+                    <div class="p-3 mb-2 bg-success text-white">Выучено слов: {{learnedWords.length}}</div>
+                    <div class="p-3 mb-2 bg-primary text-white">Осталось выучить: {{vocabulary.length}}</div>
+                  </div>
+                  <!-- Modal footer -->
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-danger waves-effect waves-light" data-dismiss="modal">Закрыть
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
@@ -155,7 +182,11 @@
         localStorage.setItem('vocabulary', JSON.stringify(this.vocabulary));
         localStorage.setItem('learnedWords', JSON.stringify(this.learnedWords));
 
-      }
+      },
+
+      showInfo: function () {
+        $('#info').modal('show');
+      },
     },
   }
 </script>
@@ -175,5 +206,15 @@
 
   .big-text {
     font-size: 5em;
+  }
+
+  .topcorner {
+    position: absolute;
+    top: 25px;
+    right: 25px;
+  }
+
+  .fa-info-circle {
+    font-size: 22px !important;
   }
 </style>
